4fe728653bc35b85644a29c79103e3c00b32e4ea
Victoria Chen <54255894+victoriaychen@users.noreply.github.com>
2022-10-16 10:33:38 -0400

    Update score when player collide with a food item

--- Workplace/Assets/Scripts/PlayerMove.cs
+++ Workplace/Assets/Scripts/PlayerMove.cs
@@ -62,7 +62,7 @@ public class PlayerMove : MonoBehaviour
 	void OnTriggerEnter2D(Collider2D other)
 	{
         if (other.gameObject.tag == "food"){
-			print("add score here");
+			gameHandlerObj.AddScore(1);
             // gameHandlerObj.AddScore(1);
 
 			// gameObject.GetComponent<AudioSource>().Play();
