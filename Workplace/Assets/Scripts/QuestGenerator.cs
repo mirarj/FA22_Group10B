d36e9358fb5cade9500917e568cf51299719e746
mirarj <44106058+mirarj@users.noreply.github.com>
2022-10-18 16:22:30 -0400

    empty recipe box and gameover scene

--- Workplace/Assets/Scripts/QuestGenerator.cs
+++ Workplace/Assets/Scripts/QuestGenerator.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCollision : MonoBehaviour
+public class QuestGenerator : MonoBehaviour
 {
-    public int currSP;
     // Start is called before the first frame update
     void Start()
     {
-        currSP = -1;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void OnTriggerEnter2D(Collider2D other) {
-        GetComponentInParent<PickupSpawner>().RespawnOne(transform);
-
     }
 }
